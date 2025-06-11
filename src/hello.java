72f4a18c13b233ba4ca6f98f29634d0c1d28008a
juhebaji1011 <juhebaji@gmail.com>
2025-06-11 22:47:30 +0530

    hello created

--- src/hello.java
+++ src/hello.java
@@ -3,5 +3,9 @@ public class hello {
     public static void main(String[] args)
     {
         System.out.println("Jasu");
+        System.out.println("Imran");
+
+        System.out.println("Yash");
+
     }
 }
